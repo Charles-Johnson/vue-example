25565d8bbd5c4403fc19fc7e347e70dff02bb1c9
Charles Johnson <charlesthomasjohnson0@gmail.com>
2019-07-27 14:33:58 +0900

    Set up iView header+content layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Row>
-      <i-Col span="8">
-        <p>To Do</p>
-        <Divider/>
-        <draggable v-model="toDoArray" group="tasks" @start="drag=true" @end="drag=false">
-          <div v-for="element in toDoArray" :key="element.id">{{element.name}}</div>
-        </draggable>
-      </i-Col>
-      <i-Col span="8">
-        <p>In Progress</p>
-        <Divider/>
-        <draggable v-model="inProgressArray" group="tasks" @start="drag=true" @end="drag=false">
-          <div v-for="element in inProgressArray" :key="element.id">{{element.name}}</div>
-        </draggable>
-      </i-Col>
-      <i-Col span="8">
-        <p>Done</p>
-        <Divider/>
-        <draggable v-model="finishedArray" group="tasks" @start="drag=true" @end="drag=false">
-          <div v-for="element in finishedArray" :key="element.id">{{element.name}}</div>
-        </draggable>
-      </i-Col>
-    </Row>
-  </div>
+    <Layout id="app" class="layout">
+      <Header>
+        <h1 id="header">To-do List for Vue.js To-do List</h1>
+      </Header>
+      <Content>
+        <Row>
+          <i-Col span="8">
+            <p>To Do</p>
+            <Divider/>
+            <draggable v-model="toDoArray" group="tasks" @start="drag=true" @end="drag=false">
+              <div v-for="element in toDoArray" :key="element.id">{{element.name}}</div>
+            </draggable>
+          </i-Col>
+          <i-Col span="8">
+            <p>In Progress</p>
+            <Divider/>
+            <draggable v-model="inProgressArray" group="tasks" @start="drag=true" @end="drag=false">
+              <div v-for="element in inProgressArray" :key="element.id">{{element.name}}</div>
+            </draggable>
+          </i-Col>
+          <i-Col span="8">
+            <p>Done</p>
+            <Divider/>
+            <draggable v-model="finishedArray" group="tasks" @start="drag=true" @end="drag=false">
+              <div v-for="element in finishedArray" :key="element.id">{{element.name}}</div>
+            </draggable>
+          </i-Col>
+        </Row>
+      </Content>
+    </Layout>
 </template>
 
 <script>
@@ -52,6 +56,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#header {
+  font-size: large;
+  color: white;
 }
 </style>
